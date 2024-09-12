71ec7962b68c2feda37067e795c959f1a8cedade
longmaker2 <l.deng@alustudent.com>
2024-09-12 12:54:58 +0200

    updated

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,7 +10,7 @@ const NavBar: React.FC = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="navbar-logo">Logo</div>
-      <button className="navbar-toggler" type="button" onClick={toggleMenu}>
+      <button className="navbar-toggler" type="button" onClick={toggleMenu} title="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`}>
